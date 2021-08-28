6a4e4204f1428fb1df469e5e5d7e626607141ef7
jellis56 <62670219+jellis56@users.noreply.github.com>
2021-08-28 12:01:31 -0400

    Update HelloWorld.java

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -5,6 +5,12 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello from Eclipse");
+		System.out.println("Hello from GitHub");
+		
+	}
+	public static void heyNow() {
+		String Msg = "Hey Now!";
+		System.out.println(Msg);
 	}
 
 }
